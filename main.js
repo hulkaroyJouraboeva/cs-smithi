338ccd7061c87cd5dcbe3e5f5d39bcdc3a7f97e5
irnatus <stanchenw@gmail.com>
2022-06-22 23:26:40 -0400

    added extension icon and testing oauth

--- main.js
+++ main.js
@@ -3,10 +3,25 @@
 (function showEvents() {
   const eventsURL = 'https://www.googleapis.com/calendar/v3/calendars/c_9qtn0db3bh1leu1t6ueun0jutk@group.calendar.google.com/events';
 
-  fetch(`${eventsURL}`)
-    .then((data) => data.json())
-    .then((formattedData) => console.log(formattedData))
-    .catch((error) => console.log('Error occurred: ', error));
+  chrome.identity.getAuthToken({ interactive: true }, function(token) {
+    console.log(token);
+
+    const fetch_options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      }
+    };
+    
+    fetch(`${eventsURL}`, fetch_options)
+      .then((data) => data.json())
+      .then((formattedData) => console.log(formattedData))
+      .catch((error) => console.log('Error occurred: ', error));
+
+  });
+
+  // 122366426078-qkivld820lbnq5pt18vaoaf2ddb9ehia.apps.googleusercontent.com
 
 })()
 
