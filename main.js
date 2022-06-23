f326cbc8cbc18e2c8cc7d373654d43b1d358a0fe
irnatus <stanchenw@gmail.com>
2022-06-23 00:00:25 -0400

    trying oauth

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 // listen to a click event on both buttons
 
-(function showEvents() {
-  const eventsURL = 'https://www.googleapis.com/calendar/v3/calendars/c_9qtn0db3bh1leu1t6ueun0jutk@group.calendar.google.com/events';
+// (function showEvents() {
+//   const eventsURL = 'https://www.googleapis.com/calendar/v3/calendars/c_9qtn0db3bh1leu1t6ueun0jutk@group.calendar.google.com/events';
 
-  chrome.identity.getAuthToken({ interactive: true }, function(token) {
-    console.log(token);
+//   chrome.identity.getAuthToken({ interactive: true }, function(token) {
+//     console.log(token);
 
-    const fetch_options = {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      }
-    };
+//     const fetch_options = {
+//       method: 'GET',
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//         'Content-Type': 'application/json',
+//       }
+//     };
     
-    fetch(`${eventsURL}`, fetch_options)
-      .then((data) => data.json())
-      .then((formattedData) => console.log(formattedData))
-      .catch((error) => console.log('Error occurred: ', error));
+//     fetch(`${eventsURL}`, fetch_options)
+//       .then((data) => data.json())
+//       .then((formattedData) => console.log(formattedData))
+//       .catch((error) => console.log('Error occurred: ', error));
 
-  });
+//   });
 
-  // 122366426078-qkivld820lbnq5pt18vaoaf2ddb9ehia.apps.googleusercontent.com
+//   // 122366426078-qkivld820lbnq5pt18vaoaf2ddb9ehia.apps.googleusercontent.com
 
-})()
+// })()
 
 
 
---
+++ oauth.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+  chrome.identity.getAuthToken({interactive: true}, function(token) {
+    console.log(token);
+    const eventsURL = 'https://www.googleapis.com/calendar/v3/calendars/c_9qtn0db3bh1leu1t6ueun0jutk@group.calendar.google.com/events';
+
+
+    const fetch_options = {
+      method: 'GET',
+      async: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      }
+    };
+    
+    fetch(`${eventsURL}`, fetch_options)
+      .then((data) => data.json())
+      .then((formattedData) => console.log(formattedData))
+      .catch((error) => console.log('Error occurred: ', error));
+  });
+};
+  
+
+// window.onload = function() {
+//   document.querySelector('button').addEventListener('click', function() {
+//     chrome.identity.getAuthToken({interactive: true}, function(token) {
+//       let init = {
+//         method: 'GET',
+//         async: true,
+//         headers: {
+//           Authorization: 'Bearer ' + token,
+//           'Content-Type': 'application/json'
+//         },
+//         'contentType': 'json'
+//       };
+
+//       fetch('https://people.googleapis.com/v1/contactGroups/all?maxMembers=20&key=API_KEY', init)
+//           .then((response) => response.json())
+//           .then(function(data) {
+//             console.log(data)
+//           });
+//     });
+//   });
+// };
